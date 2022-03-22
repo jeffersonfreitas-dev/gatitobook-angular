5e6f4cc79e43bf3d2de9e457df72f96bba447642
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-03-22 20:31:55 -0300

    Rotas home e component login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //Rota padrão Home quando a rota for vazia
+    path:'', component:HomeComponent,
+    children: [
+      //Rota children padrão em Home para o Login quando a rota for vazia
+      {path:'', component:LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
